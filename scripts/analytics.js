f5c727c928978958bba7809d4afff208691730cb
trekare <trekare@gmail.com>
2015-11-09 19:23:38 -0500

    [added] Landing page

---
+++ scripts/analytics.js
@@ -0,0 +1,9 @@
++function(l,y,t,i,c,s) {
+    l['LocalyticsGlobal'] = i;
+    l[i] = function() { (l[i].q = l[i].q || []).push(arguments) };
+    l[i].t = +new Date;
+    (s = y.createElement(t)).type = 'text/javascript';
+    s.src = '//web.localytics.com/v3/localytics.js';
+    (c = y.getElementsByTagName(t)[0]).parentNode.insertBefore(s, c);
+    ll('init', '2a8c1a0ed02b9a3aa68270e-74ab6140-8727-11e5-9c3c-003e57fecdee', {} /* Options */);
+}(window, document, 'script', 'll');
